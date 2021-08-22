9f1eed6b873a15c5704552cf33011643acff6e2a
pattex16 <iacobuccivalerio@gmail.com>
2021-08-22 17:40:31 +0200

    modified:   ipython/profile_default/history.sqlite 	modified:   ipython/profile_default/ipython_config.py 	modified:   polybar/config

--- ipython/profile_default/ipython_config.py
+++ ipython/profile_default/ipython_config.py
@@ -1,6 +1,22 @@
+from os.path import expanduser
+from os import system
+from os import environ
+home = expanduser("~")
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalIPythonApp.display_banner = False
+c.InteractiveShellApp.exec_lines = [
+    'import numpy as np',
+]
+c.InteractiveShellApp.exec_files = [
+]
+c.InteractiveShell.confirm_exit = True
+c.InteractiveShell.editor = 'vim'
 
-import os
-os.system("tmux rename-window ipy")
-os.system("tmux set-window-option window-status-current-style 'fg=black bg=green'")
-os.system("tmux set-window-option window-status-style 'fg=green'")
+
+if "TMUX" in environ:
+    system("tmux set-window-option window-status-current-style 'fg=black bg=green'; tmux set-window-option window-status-style 'fg=green' ; tmux rename-window ipython")
+
+# c.PromptManager.in_template  = 'In [\#]: '
+# c.PromptManager.in2_template = '   .\D.: '
+# c.PromptManager.out_template = 'Out[\#]: '
+# c.PromptManager.justify = True
