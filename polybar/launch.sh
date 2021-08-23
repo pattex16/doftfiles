1538e78526654fc8d0bb586d4dba4d43494d1458
pattex16 <iacobuccivalerio@gmail.com>
2021-08-23 02:48:57 +0200

    modified:   bspwm/bspwmrc 	modified:   dunst/dunstrc 	modified:   kupfer/mnemonics.pickle 	new file:   polybar/battery-combined-udev 	modified:   polybar/config 	new file:   polybar/launch.sh 	new file:   polybar/polybar-now-playing

---
+++ polybar/launch.sh
@@ -0,0 +1,14 @@
+CPID=$(pgrep -x polybar)
+
+if [ -n "${CPID}" ] ; then
+  kill -TERM ${CPID}
+fi
+
+# add window titles
+# using bspc query here to get monitors in the same order bspwm sees them
+for m in $( bspc query -M --names ); do
+    index=$((index + 1))
+    export P_BSPWM_WINDOW_CMD="tail ${HOME}/.cache/bspwm_windows_${index}.txt"
+
+    MONITOR=$m polybar --reload bspwm_gnome &
+done
